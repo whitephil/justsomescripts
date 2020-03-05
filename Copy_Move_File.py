d1fecc5ebbe346f40dc5201989944ec8742cbc00
outpw <philip.white@colorado.edu>
2020-03-05 12:08:34 -0700

    forgot a line in copy script

--- Copy_Move_File.py
+++ Copy_Move_File.py
@@ -6,6 +6,8 @@ os.chdir('C:\\Users\\phwh9568\\data')
 
 copyfile('readme.txt', 'Colorado_Microsoft_Footprints\\readme.txt')
 
+folders = glob.glob('Colorado_Microsoft_Footprints\\*')
+
 for folder in folders:
     try:
         copyfile('readme.txt', folder+'\\readme.txt')
